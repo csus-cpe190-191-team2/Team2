dddb05d0392480783350428d2611f3a1c32185df
Ryan Aboueljoud <r.aboueljoud@gmail.com>
2022-03-01 23:17:51 -0800

    Added input handling for DC motor control

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import evdev
+import motor
 # from evdev import InputDevice,categorize,ecodes
 
 
@@ -6,29 +7,27 @@ class Input:
     def __init__(self):
         self.gamepad = None
         self.deviceName = None
+        self.motor_control = motor.MotorControl()
         path = "/dev/input"
 
         # Get list of currently connected input devices
         device_list = [evdev.InputDevice(path) for path in evdev.list_devices()]
-        
+
         # Attach to device with keyboard input
         for device in device_list:
-            print(device.name)
-            name = device.name
-            string1 = 'Keyboard'
-            string2 = 'keyboard'
-            if string1 in name or string2 in name:
+            if "Keyboard" in device.name or "keyboard" in device.name:
                 self.gamepad = evdev.InputDevice(device)
                 self.deviceName = device
         if self.gamepad is not None:
             print("Connected to: ", self.gamepad)
         else:
             print("No Bluetooth Device Connected!")
+            motor.destroy()
 
     # Handle controller input
     def input_listen(self):
         curr_input = None        # Temp var holds current input value
-        button_map = None    # Adjust button map based on currently connected input device
+        button_map = None        # Adjust button map based on currently connected input device
 
         ipega_mapping = {
             103: "UP",
@@ -46,18 +45,18 @@ class Input:
         }
 
         eightbitdo_mapping = {
-            46 : "UP",
-            32 : "DOWN",
-            18 : "LEFT",
-            33 : "RIGHT",
-            34 :  "A",
-            35 :  "X",
-            23 :  "Y",
-            36 :  "B",
-            49 :  "SELECT",
-            24 :  "START",
-            37 :  "LEFT TRIGGER",
-            50 :  "RIGHT TRIGGER"
+            46: "UP",
+            32: "DOWN",
+            18: "LEFT",
+            33: "RIGHT",
+            34:  "A",
+            35:  "X",
+            23:  "Y",
+            36:  "B",
+            49:  "SELECT",
+            24:  "START",
+            37:  "LEFT TRIGGER",
+            50:  "RIGHT TRIGGER"
         }
 
         # Assign button map to appropriate input device
@@ -75,12 +74,49 @@ class Input:
                 # print("Event Type: ", event.type, "\tEvent Code: ", event.code, '\tEvent Value: ', event.value)# DEBUG
                 if event.value == 1:    # 1 means button is pressed, 0 is released
                     curr_input = button_map.get(event.code)
+
+                    print(curr_input)    # Temp output for DEBUG
+                    if curr_input == "UP":
+                        self.motor_control.forward()
+                    if curr_input == "DOWN":
+                        self.motor_control.backward()
+                    if curr_input == "LEFT":
+                        self.motor_control.turn_left()
+                    if curr_input == "RIGHT":
+                        self.motor_control.turn_right()
+                    if curr_input == "A":
+                        self.motor_control.speed_up()
+                    if curr_input == "X":
+                        self.motor_control.speed_down()
+                    if curr_input == "Y":
+                        self.motor_control.stop()
+                    if curr_input == "B":
+                        print("No Function Yet...")
+                        #  Do something?
+                    if curr_input == "SELECT":
+                        print("No Function Yet...")
+                        #  Do something?
+                    # Exit manual mode if START button is pressed
                     if curr_input == "START":
+                        self.motor_control.stop()
+                        motor.destroy()
                         self.gamepad.close()
                         quit()
-                    print(curr_input)    # Temp output for DEBUG, eventually handle motor control here...
+                    if curr_input == "Ltrigger":
+                        print("No Function Yet...")
+                        #  Do something?
+                    if curr_input == "Rtrigger":
+                        print("No Function Yet...")
+                        #  Do something?
+
+                    # Temp DEBUG output
+                    print("Left: ", self.motor_control.left_duty, "\tRight: ", self.motor_control.right_duty)
+
         except OSError as e:
             print("Controller Disconnected: ", e)
             # Stop Motors
         except AttributeError as e:
             print("Button map configuration error: ", e)
+        finally:
+            motor.destroy()
+            self.gamepad.close()
--- motor.py
+++ motor.py
@@ -1,1 +1,173 @@
-print("Motor")
+import RPi.GPIO as GPIO
+import math
+from time import sleep
+
+# GPIO Pins for Motor Driver Inputs
+Motor1A = 16        # Green Wire 16
+Motor2A = 18        # Blue Wire 18
+MotorA_PWM = 24     # 22
+Motor1B = 19        # Green Wire 19
+Motor2B = 26        # Blue Wire 26
+MotorB_PWM = 22     # 24
+Stby = 15
+
+
+# Reset ports used for GPIO
+def destroy():
+    GPIO.cleanup()
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)  # GPIO Pin Numbering Scheme
+    GPIO.setup(Motor1A, GPIO.OUT)
+    GPIO.setup(Motor2A, GPIO.OUT)
+    GPIO.setup(MotorA_PWM, GPIO.OUT)
+
+    GPIO.setup(Motor1B, GPIO.OUT)
+    GPIO.setup(Motor2B, GPIO.OUT)
+    GPIO.setup(MotorB_PWM, GPIO.OUT)
+    GPIO.setup(Stby, GPIO.OUT)
+
+
+class MotorControl:
+    def __init__(self):
+        self.MAX_DUTY = 90   # Limit max motor speed to avoid damage
+        self.left_duty = 0
+        self.right_duty = 0
+        setup()              # Setup GPIO
+        # Initiate PWM
+        self.left_motor = GPIO.PWM(MotorA_PWM, 100)
+        self.right_motor = GPIO.PWM(MotorB_PWM, 100)
+        self.left_motor.start(self.left_duty)
+        self.right_motor.start(self.right_duty)
+        # Set motor configuration to spin forwards
+        GPIO.output(Motor1A, GPIO.HIGH)
+        GPIO.output(Motor2A, GPIO.LOW)
+        GPIO.output(Motor1B, GPIO.HIGH)
+        GPIO.output(Motor2B, GPIO.LOW)
+        # Keep motor controller in standby mode to start
+        GPIO.output(Stby, GPIO.LOW)
+
+    # Configure motors to move in a forward direction
+    def forward(self):
+        if not GPIO.input(Stby):    # Motors disabled when not active
+            # Stby: Allow H-bridges to work when high
+            # (has a pull down resistor must be actively pulled HIGH)
+            GPIO.output(Stby, GPIO.HIGH)
+
+        # Sync motors
+        avg_duty = max(self.left_duty, self.right_duty)
+        self.left_duty = self.right_duty = avg_duty
+        # Reduce motor speeds gradually to prepare for forward motion
+        for i in range(avg_duty, -1, -1):
+            self.left_motor.ChangeDutyCycle(i)
+            self.right_motor.ChangeDutyCycle(i)
+            sleep(0.01)  # Sleep 1ms
+
+        # Set motor configuration to spin forwards
+        GPIO.output(Motor1A, GPIO.HIGH)
+        GPIO.output(Motor2A, GPIO.LOW)
+        GPIO.output(Motor1B, GPIO.HIGH)
+        GPIO.output(Motor2B, GPIO.LOW)
+
+        # Increase motor speeds gradually to original speed before direction change
+        for i in range(0, avg_duty+1, +1):
+            self.left_motor.ChangeDutyCycle(i)
+            self.right_motor.ChangeDutyCycle(i)
+            sleep(0.01)  # Sleep 1ms
+
+    # Configure motors to move in a backward direction
+    def backward(self):
+        if not GPIO.input(Stby):  # Motors disabled when not active
+            # Stby: Allow H-bridges to work when high
+            # (has a pull down resistor must be actively pulled HIGH)
+            GPIO.output(Stby, GPIO.HIGH)
+
+        # Normalize motor data, find average duty cycle and sync motors
+        avg_duty = max(self.left_duty, self.right_duty)
+        self.left_duty = self.right_duty = avg_duty
+
+        # Reduce motor speeds gradually to prepare for forward motion
+        for i in range(avg_duty, -1, -1):
+            self.left_motor.ChangeDutyCycle(i)
+            self.right_motor.ChangeDutyCycle(i)
+            sleep(0.01)  # Sleep 1ms
+
+        # Set motor configuration to spin backwards
+        GPIO.output(Motor1A, GPIO.LOW)
+        GPIO.output(Motor2A, GPIO.HIGH)
+        GPIO.output(Motor1B, GPIO.LOW)
+        GPIO.output(Motor2B, GPIO.HIGH)
+
+        # Increase motor speeds gradually to original speed before direction change
+        for i in range(0, avg_duty+1, +1):
+            self.left_motor.ChangeDutyCycle(i)
+            self.right_motor.ChangeDutyCycle(i)
+            sleep(0.01)  # Sleep 1ms
+
+    # Reduce left motor by half to turn left
+    def turn_left(self):
+        if not GPIO.input(Stby):  # Motors disabled when not active
+            # Stby: Allow H-bridges to work when high
+            # (has a pull down resistor must be actively pulled HIGH)
+            GPIO.output(Stby, GPIO.HIGH)
+
+        self.left_duty -= 40
+        self.left_motor.ChangeDutyCycle(self.left_duty)
+
+    # Reduce right motor by half to turn right
+    def turn_right(self):
+        if not GPIO.input(Stby):  # Motors disabled when not active
+            # Stby: Allow H-bridges to work when high
+            # (has a pull down resistor must be actively pulled HIGH)
+            GPIO.output(Stby, GPIO.HIGH)
+
+        self.right_duty -= 40
+        self.right_motor.ChangeDutyCycle(self.right_duty)
+
+    # ### NEEDS WORK: Increase speed of motor
+    def speed_up(self):
+        if not GPIO.input(Stby):  # Motors disabled when not active
+            # Stby: Allow H-bridges to work when high
+            # (has a pull down resistor must be actively pulled HIGH)
+            GPIO.output(Stby, GPIO.HIGH)
+
+        if (self.left_duty + 5) < self.MAX_DUTY:
+            self.left_duty = math.floor(self.left_duty + 5)
+        if (self.right_duty + 5) < self.MAX_DUTY:
+            self.right_duty = math.floor(self.right_duty + 5)
+        self.left_motor.ChangeDutyCycle(self.left_duty)
+        self.right_motor.ChangeDutyCycle(self.right_duty)
+
+    # ### NEEDS WORK: Decrease speed of motor
+    def speed_down(self):
+        # If in standby mode and at least one motor duty cycle is above zero, activate motor controller
+        if not GPIO.input(Stby) and ((self.left_duty != 0) or (self.right_duty != 0)):
+            # Stby: Allow H-bridges to work when high
+            # (has a pull down resistor must be actively pulled HIGH)
+            GPIO.output(Stby, GPIO.HIGH)
+
+        self.left_duty = math.floor(self.left_duty / 1.5)
+        self.right_duty = math.floor(self.right_duty / 1.5)
+        self.left_motor.ChangeDutyCycle(self.left_duty)
+        self.right_motor.ChangeDutyCycle(self.right_duty)
+        # If speed is zero place controller in standby mode to conserve power
+        if (self.left_duty == 0) and (self.right_duty == 0):
+            GPIO.output(Stby, GPIO.LOW)
+
+    def stop(self):
+        if not GPIO.input(Stby):  # Motors disabled when not active
+            # Stby: Allow H-bridges to work when high
+            # (has a pull down resistor must be actively pulled HIGH)
+            GPIO.output(Stby, GPIO.HIGH)
+
+        avg_duty = math.floor((self.left_duty + self.right_duty) / 2)
+
+        # Reduce motor speeds gradually to prepare for forward motion
+        for i in range(avg_duty, -1, -1):
+            self.left_motor.ChangeDutyCycle(i)
+            self.right_motor.ChangeDutyCycle(i)
+            sleep(0.01)  # Sleep 1ms
+
+        # If place motor controller in standby mode to conserve power
+        GPIO.output(Stby, GPIO.LOW)
