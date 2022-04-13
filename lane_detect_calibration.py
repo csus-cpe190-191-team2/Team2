c3f35f0274483b4f2538be9fd2c3e27047c79429
Ryan Aboueljoud <r.aboueljoud@gmail.com>
2022-04-12 18:19:49 -0700

    updated lane_detect_calibration.py to now save and load slider values from disk

--- lane_detect_calibration.py
+++ lane_detect_calibration.py
@@ -16,6 +16,7 @@ def get_img(display=False, size=[480, 240]):
 
     return img
 
+
 def thresholding(img, points):
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_blue = np.array(points[0])  # Blue: 81, 54, 140
@@ -31,13 +32,15 @@ def warp_image(img, points, w, h, inv=False):
         matrix = cv2.getPerspectiveTransform(pts2, pts1)
     else:
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    img_warp = cv2.warpPerspective(img, matrix, (w,h))
+    img_warp = cv2.warpPerspective(img, matrix, (w, h))
     return img_warp
+
 
 def draw_points(img, points):
     for x in range(4):
-        cv2.circle(img,(int(points[x][0]), int(points[x][1])), 15, (0,0,255), cv2.FILLED)
+        cv2.circle(img, (int(points[x][0]), int(points[x][1])), 15, (0, 0, 255), cv2.FILLED)
     return img
+
 
 def get_histogram(img, display=False, region=1):
 
@@ -45,11 +48,6 @@ def get_histogram(img, display=False, region=1):
         hist_vals = np.sum(img, axis=0)
     else:
         hist_vals = np.sum(img[img.shape[0]//region:,:], axis=0)
-
-    # # Normalize values to max out at 255
-    # for i, val in enumerate(hist_vals):
-    #     if val > 255:
-    #         hist_vals[i] = 255
 
     if display:
         img_hist = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
@@ -61,29 +59,35 @@ def get_histogram(img, display=False, region=1):
         return hist_vals
 
 
-def initialize_trackbars(init_trackbar_vals, w_t=480, h_t=240): # w_t=360
+def initialize_trackbars(init_trackbar_vals, w_t=480, h_t=240):
+    init_warp = init_trackbar_vals[0].astype(int)
+    init_hsv = init_trackbar_vals[1]
+    init_tools = init_trackbar_vals[2]
+
     # Image warp sliders
     cv2.namedWindow("Warp Sliders")
     cv2.resizeWindow("Warp Sliders", w_t, h_t)
-    cv2.createTrackbar("Width Top", "Warp Sliders", init_trackbar_vals[0], w_t//2, empty)
-    cv2.createTrackbar("Height Top", "Warp Sliders", init_trackbar_vals[1], h_t, empty)
-    cv2.createTrackbar("Width Bottom", "Warp Sliders", init_trackbar_vals[2], w_t // 2, empty)
-    cv2.createTrackbar("Height Bottom", "Warp Sliders", init_trackbar_vals[3], h_t, empty)
+    cv2.createTrackbar("Width Top", "Warp Sliders", init_warp[0, 0], w_t//2, empty)
+    cv2.createTrackbar("Height Top", "Warp Sliders", init_warp[1, 1], h_t, empty)
+    cv2.createTrackbar("Width Bottom", "Warp Sliders", init_warp[2, 0], w_t//2, empty)
+    cv2.createTrackbar("Height Bottom", "Warp Sliders", init_warp[3, 1], h_t, empty)
 
     # Image threshold sliders
     cv2.namedWindow("HSV Sliders")
     cv2.resizeWindow("HSV Sliders", w_t, h_t)
-    cv2.createTrackbar("HUE Min", "HSV Sliders", init_trackbar_vals[4], 179, empty)
-    cv2.createTrackbar("HUE Max", "HSV Sliders", init_trackbar_vals[5], 179, empty)
-    cv2.createTrackbar("SAT Min", "HSV Sliders", init_trackbar_vals[6], 255, empty)
-    cv2.createTrackbar("SAT Max", "HSV Sliders", init_trackbar_vals[7], 255, empty)
-    cv2.createTrackbar("VALUE Min", "HSV Sliders", init_trackbar_vals[8], 255, empty)
-    cv2.createTrackbar("VALUE Max", "HSV Sliders", init_trackbar_vals[9], 255, empty)
+    cv2.createTrackbar("HUE Min", "HSV Sliders", init_hsv[0, 0], 179, empty)
+    cv2.createTrackbar("HUE Max", "HSV Sliders", init_hsv[1, 0], 179, empty)
+    cv2.createTrackbar("SAT Min", "HSV Sliders", init_hsv[0, 1], 255, empty)
+    cv2.createTrackbar("SAT Max", "HSV Sliders", init_hsv[1, 1], 255, empty)
+    cv2.createTrackbar("VALUE Min", "HSV Sliders", init_hsv[0, 2], 255, empty)
+    cv2.createTrackbar("VALUE Max", "HSV Sliders", init_hsv[1, 2], 255, empty)
 
+    # Image Hough transform and save sliders
     cv2.namedWindow("Tools")
     cv2.resizeWindow("Tools", w_t, h_t)
-    cv2.createTrackbar("Hough Threshold", "Tools", init_trackbar_vals[10], 250, empty)
-    cv2.createTrackbar("Hough Max Gap", "Tools", init_trackbar_vals[11], 500, empty)
+    cv2.createTrackbar("Hough Threshold", "Tools", init_tools[0], 250, empty)
+    cv2.createTrackbar("Hough Max Gap", "Tools", init_tools[1], 500, empty)
+
     cv2.createTrackbar("Save Settings", "Tools", 0, 1, empty)
 
 
@@ -106,7 +110,7 @@ def val_trackbars(w_t=480, h_t=240):
     hough_max_gap = cv2.getTrackbarPos("Hough Max Gap", "Tools")
 
     warp = np.float32([(width_top, height_top), (w_t - width_top, height_top),
-                         (width_bottom, height_bottom), (w_t - width_bottom, height_bottom)])
+                       (width_bottom, height_bottom), (w_t - width_bottom, height_bottom)])
     thresh = np.int_([lower, upper])
     hough = np.int_([hough_thresh, hough_max_gap])
     save = cv2.getTrackbarPos("Save Settings", "Tools")
@@ -153,7 +157,20 @@ if __name__ == '__main__':
         img = get_img()
         h_t, w_t, ch = img.shape
 
-    init_trackbar_vals = [50, 132, 0, 187, 81, 128, 54, 255, 140, 255, 10, 150]  # DEFAULT: 0,0,0,200
+    # Saved variable paths
+    warp_fn = 'vars/warp_points.txt'
+    thresh_fn = 'vars/thresh_points.txt'
+    hough_fn = 'vars/hough_points.txt'
+
+    # Read in variable variable values saved to disk
+    with open(warp_fn, 'r') as file:
+        warp_points = np.loadtxt(file)
+    with open(thresh_fn, 'r') as file:
+        thresh_points = np.loadtxt(file, dtype=int)
+    with open(hough_fn, 'r') as file:
+        hough_points = np.loadtxt(file, dtype=int)
+
+    init_trackbar_vals = [warp_points, thresh_points, hough_points]  # DEFAULT: 0,0,0,200
     initialize_trackbars(init_trackbar_vals, w_t, h_t)
 
     frame_counter = 0
@@ -163,8 +180,20 @@ if __name__ == '__main__':
         img_result = img.copy()
         warp_points, thresh_points, hough_points, save_points = val_trackbars()
 
+        # If save switch is on, write slider values to disk
         if save_points:
-            print("SAVE")
+            a = warp_points
+            b = thresh_points
+            c = hough_points
+            np.savetxt(warp_fn, a, fmt='%s')
+            np.savetxt(thresh_fn, b, fmt='%s')
+            np.savetxt(hough_fn, c, fmt='%s')
+            with open(warp_fn, 'r') as file:
+                warp_points = np.loadtxt(file)
+            with open(thresh_fn, 'r') as file:
+                thresh_points = np.loadtxt(file, dtype=int)
+            with open(hough_fn, 'r') as file:
+                hough_points = np.loadtxt(file, dtype=int)
 
         # ## Threshold and warp image
         img_thresh = thresholding(img.copy(), thresh_points)
@@ -187,27 +216,20 @@ if __name__ == '__main__':
         img_lane_color[:] = 0, 255, 0
         img_lane_color = cv2.bitwise_and(img_inv_warp, img_lane_color)
         img_result = cv2.addWeighted(img_result, 1, img_lane_color, 1, 0)
+
+        # Draw FPS counter
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
         timer = cv2.getTickCount()
         cv2.putText(img_result, 'FPS ' + str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3);
-
-
-        # img_result = warp_image(img_warp, points, w_t, h_t, inv=True)
-        # img_result = cv2.addWeighted(img, 0.6, img_result, 0.8, 0)
 
         img = draw_points(img, warp_points)  # Draw warp points
 
         # Display Image Stack
         imgStacked = stack_images(1, ([img, img_thresh, edges],
-                                     [img_result, img_hist, img_lines]))
+                                      [img_result, img_hist, img_lines]))
         cv2.imshow('ImageStack', imgStacked)
-        # cv2.imshow("img", img)
-        # cv2.imshow("Threshold", img_thresh)
-        # cv2.imshow("Edges", edges)
-        # cv2.imshow("Warp", img_lines)
-        # cv2.imshow("Result", img_result)
-        # cv2.imshow("Histogram", img_hist)
 
+        # Delay 1ms per frame; Quit via ctl+c in terminal  or 'q' in window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
