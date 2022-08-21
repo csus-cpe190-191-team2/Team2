4b708454b91e1b8d1f06e0d9a2fc9ce9ccb53ae3
Ryan Aboueljoud <r.aboueljoud@gmail.com>
2022-08-20 17:10:25 -0700

    Merged controller.py with device_finder.py and removed depreciated files

--- controller.py
+++ controller.py
@@ -1,63 +1,96 @@
 import evdev
-import motor
+
+path = "/dev/input"
 
 
-class Input:
-    def __init__(self):
+class Controller:
+    def __init__(self, dev=0):
         self.gamepad = None
-        self.deviceName = None
-        self.button_map = {}
-        self.motor_control = motor.MotorControl()
-        path = "/dev/input"
+        self.device_name = None
+        self.map = {}
+        self.special_controller = False
 
-        # Get list of currently connected input devices
+        # Attach to device
+        self.set_controller(dev)
+
+        # Attaches mapping to input device
+        self.set_map()
+
+    def kill_device(self):
+        self.gamepad.close()
+
+    def set_controller(self, dev=0):
         device_list = [evdev.InputDevice(path) for path in evdev.list_devices()]
-        # Attach to device with keyboard input
-        for device in device_list:
-            if "Keyboard" in device.name or "keyboard" in device.name\
-                    or "Controller" in device.name or "controller" in device.name:
-                self.gamepad = evdev.InputDevice(device)
-                self.deviceName = device
-        if self.gamepad is not None:
-            print("Connected to: ", self.gamepad)
-        else:
-            print("No Bluetooth Device Connected!")
-            motor.destroy()
+        if dev == 0:
+            i = 1
+            for device in device_list:
+                print(i, device)
+                i += 1
+            x = input('Device #: ')
+            i = 1
+            for device in device_list:
+                if int(x) == i:
+                    self.gamepad = evdev.InputDevice(device)
+                    self.device_name = device.name
+                i += 1
+        elif dev == 1:
+            for device in device_list:
+                if "8BitDo" in device.name:
+                    self.gamepad = evdev.InputDevice(device)
+                    self.device_name = device.name
+            if self.gamepad == None:
+                print("8BitDo not found...exiting")
+                quit()
+        elif dev == 2:
+            for device in device_list:
+                if "Keyboard" in device.name:
+                    self.gamepad = evdev.InputDevice(device)
+                    self.device_name = device.name
+            if self.gamepad == None:
+                print("Keyboard not found...exiting")
+                quit()
+        elif dev == 3:
+            for device in device_list:
+                if "controller" in device.name.lower():
+                    self.gamepad = evdev.InputDevice(device)
+                    self.device_name = device.name
+            if self.gamepad == None:
+                print("Controller not found...exiting")
+                quit()
 
-    # Handle controller input
-    def input_listen(self):
-        curr_input = None  # Temp var holds current input value
+        print("Connected to: ", self.gamepad)
 
-        ipega_mapping = {
-            104: "UP",
-            109: "DOWN",
-            106: "LEFT",
-            107: "RIGHT",
-            37: "A",
-            51: "X",
-            24: "Y",
-            38: "B",
-            20: "SELECT",
-            22: "START",
-            17: "LEFT TRIGGER",
-            26: "RIGHT TRIGGER"
-        }
-
+    # Handle controller mapping
+    def set_map(self):
+        print("Acquiring map...")
         eightbitdo_mapping = {
-            47: "UP",
-            33: "DOWN",
-            19: "LEFT",
-            34: "RIGHT",
-            35: "A",
-            36: "X",
-            24: "Y",
-            37: "B",
-            50: "SELECT",
-            25: "START",
-            38: "LEFT TRIGGER",
-            51: "RIGHT TRIGGER"
+            46: "UP",
+            32: "DOWN",
+            18: "LEFT",
+            33: "RIGHT",
+            34: "A",
+            35: "X",
+            23: "Y",
+            36: "B",
+            49: "SELECT",
+            24: "START",
+            37: "LEFT TRIGGER",
+            50: "RIGHT TRIGGER"
         }
-
+        keyboard_mapping = {
+            17: "UP",  # W
+            31: "DOWN",  # S
+            30: "LEFT",  # A
+            32: "RIGHT",  # D
+            36: "A",  # J
+            23: "X",  # I
+            22: "Y",  # U
+            37: "B",  # K
+            50: "SELECT",  # M
+            16: "START",  # Q
+            38: "LEFT TRIGGER",  # L
+            19: "RIGHT TRIGGER"  # R
+        }
         ps5_mapping = {
             16: "UP",
             18: "DOWN",
@@ -72,14 +105,39 @@ class Input:
             309: "LEFT TRIGGER",
             310: "RIGHT TRIGGER"
         }
-
-        # Assign button map to appropriate input device
-        if "Bluetooth Gamepad Keyboard" in self.deviceName.name:
-            self.button_map = ipega_mapping
-        elif "8BitDo" in self.deviceName.name:
-            self.button_map = eightbitdo_mapping
-        elif "Controller" in self.deviceName.name:
-            self.button_map = ps5_mapping
+        if "Keyboard &" in self.device_name:
+            self.map = keyboard_mapping
+            print("Keyboard mapped")
+        elif "8BitDo" in self.device_name:
+            self.map = eightbitdo_mapping
+            print("8BitDo mapped")
+        elif "Controller" in self.device_name:
+            self.map = ps5_mapping
+            self.special_controller = True
+            print("Controller mapped")
         else:
             print("Device does not match any recognized button map...")
             quit()
+
+    # Outputs latest controller input
+    def read_command(self):
+        event = self.gamepad.read_one()
+
+        if event == None:
+            return None
+
+        if self.special_controller:
+            if event.value == 1 or event.value == -1:
+                curr_input = self.map.get(event.code+event.value)
+                # print(curr_input, '\t', event.code, '\t', event.value)   # DEBUG
+            else:
+                return None
+        else:
+            if event.value == 1:
+                curr_input = self.map.get(event.code)
+                # print(curr_input, '\t', event.code)   # DEBUG
+            else:
+                return None
+
+        return curr_input
+
--- processing.py
+++
@@ -1,6 +0,0 @@
-class Brain:
-    def __init__(self):
-
-    def calculate(self, left, right, center):
-
-    def handle_distance(self):
