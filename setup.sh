bf1cb4d7cc563d9d986a07e243f613d055bf3923
Ryan Aboueljoud <r.aboueljoud@gmail.com>
2022-09-23 12:47:51 -0700

    Updated required dependencies in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 apt update -y
 apt upgrade -y
 apt install libatlas-base-dev -y
-python3 -m ensurepip --upgrade
+apt install ffmpeg libsm6 libxext6  -y
+apt install python3-pip -y
+pip3 install --upgrade setuptools pip
 pip3 install -r requirements.txt 
 echo Complete.
